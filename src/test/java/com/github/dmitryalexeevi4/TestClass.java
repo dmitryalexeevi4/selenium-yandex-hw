2726ba2e7c6bc98f62291030a26b17468a4bec7a
dmitryalexeevi4 <dmitryalexeevi4@yandex.ru>
2020-03-03 18:46:14 +0300

    first commit

---
+++ src/test/java/com/github/dmitryalexeevi4/TestClass.java
@@ -0,0 +1,38 @@
+package com.github.dmitryalexeevi4;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.slf4j.*;
+import org.testng.Assert;
+import org.testng.annotations.*;
+
+public class TestClass {
+    private static final Logger LOG = LoggerFactory.getLogger(TestClass.class);
+    private WebDriver webDriver;
+
+    @BeforeClass
+    public void initDriver() {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        webDriver = new ChromeDriver();
+        LOG.info("Инициализируем ChromeDriver");
+    }
+
+    @Test
+    public void helloWorldCheck() {
+        webDriver.get("https://yandex.ru/");
+        WebElement input = webDriver.findElement(By.name("text"));
+        input.sendKeys("руддщ цщкдв");
+        WebElement button = webDriver.findElement(By.className("search2__button"));
+        button.click();
+        WebElement expectedInput = webDriver.findElement(By.name("text"));
+        Assert.assertEquals(expectedInput.getAttribute("value"), "hello world");
+        Assert.assertEquals(webDriver.getTitle().contains("hello world"), true);
+        LOG.info("Выполнение теста");
+    }
+
+    @AfterClass
+    public void closeDriver() {
+        webDriver.quit();
+        LOG.info("Закрываем ChromeDriver");
+    }
+}
